096b2c9f1c65f6a5023ed53260b9661f4e3f0f56
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-23 21:22:06 +0530

    added feature to decide the winning condition.

--- SnakenLadder.java
+++ SnakenLadder.java
@@ -1,29 +1,38 @@
-
 public class SnakenLadder {
 
 	public static void main(String[] args) {
-		int stating_position=0;
-		int NoWay=0;
-		int Ladder=1;
-		int Snake=2;
-		int current_position=0;
-		int outcome=(int) (Math.floor(Math.random()*10)%6);
-		int option=(int) (Math.floor(Math.random()*10)%3);
-		System.out.println("Initial position of the player is::"+stating_position);
-		System.out.println("player got  "+outcome+" while rolling the dice");
-		switch(option)
-			{
-			case 1:current_position+=outcome;
-						break;
-			case 2:current_position-=outcome;
-					break;
-					
-			default:current_position+=0;
-			
-			}
-		System.out.println("players new position after rolling the dice="+current_position);
+		int stating_position = 0;
+		int current_position = 0;
+		System.out.println("Initial position of the player is::" + stating_position);
 
+		while (true) {
+			int outcome = (int) (Math.floor(Math.random() * 10) % 6);
+			int option = (int) (Math.floor(Math.random() * 10) % 3);
+
+			// option=0 means Noway 1 means Ladder 2 means snake
+			switch (option) {
+			case 1:
+				current_position += outcome;
+				break;
+			case 2:
+				if (current_position - outcome < 0) {
+					current_position = 0;
+				} else {
+					current_position -= outcome;
+				}
+				break;
+
+			default:
+				current_position += 0;
+
+			}
+			System.out.println("players new position after rolling the dice=" + current_position);
+			if (current_position == 100) {
+				break;
+			}
+		}
 	}
 
 }
+
 
