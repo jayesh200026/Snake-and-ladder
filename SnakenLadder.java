b1299a51be2e5cdafb6181aa1ae11ea9ee4a3c20
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-23 12:08:45 +0530

    Added initial position of the player

---
+++ SnakenLadder.java
@@ -0,0 +1,12 @@
+
+public class SnakenLadder {
+
+	public static void main(String[] args) {
+		int stating_position=0;
+		System.out.println("Initial position of the player is::"+stating_position);
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
