a3bcdb3226bfe6b1d75eaa3f7c26fd5fc2a50e9d
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-24 11:01:48 +0530

    resolved some error cases in previuos commit

--- SnakenLadder.java
+++ SnakenLadder.java
@@ -7,8 +7,8 @@ public class SnakenLadder {
 		int current_pos2 = 0;
 		int outcome1, option1, outcome2, option2;
 		while (true) {
-			if (current_pos2 == 100) {
-				System.out.println("Player2 is the winner");
+			if (current_pos1 == 100 || current_pos2 == 100) {
+
 				break;
 			}
 
@@ -18,12 +18,15 @@ public class SnakenLadder {
 			switch (option1) {
 			case 1:
 				if (current_pos1 + outcome1 > 100) {
+					System.out.println("Current position player1 after " + count1 + " die roll is " + current_pos1);
 					current_pos1 = current_pos1 + 0;
+					break;
 				} else {
 					current_pos1 += outcome1;
+					System.out.println("Current position player1 after " + count1 + " die roll is " + current_pos1);
+					continue;
 				}
 
-				continue;
 			case 2:
 				if (current_pos1 - outcome1 < 0) {
 					current_pos1 = 0;
@@ -39,11 +42,14 @@ public class SnakenLadder {
 			}
 			System.out.println("Current position player1 after " + count1 + " die roll is " + current_pos1);
 			if (current_pos1 == 100) {
-				System.out.println("Player1 is the winner");
+
 				break;
 			}
 
 			while (true) {
+				if (current_pos2 == 100) {
+					break;
+				}
 				outcome2 = (int) (Math.floor(Math.random() * 10) % 6);
 				option2 = (int) (Math.floor(Math.random() * 10) % 3);
 				count2 += 1;
@@ -52,11 +58,14 @@ public class SnakenLadder {
 				case 1:
 					if (current_pos2 + outcome2 > 100) {
 						current_pos2 = current_pos2 + 0;
+						System.out.println("Current position player2 after " + count2 + " die roll is " + current_pos2);
+						break;
 					} else {
 						current_pos2 += outcome2;
+						System.out.println("Current position player2 after " + count2 + " die roll is " + current_pos2);
+						continue;
 					}
 
-					continue;
 				case 2:
 					if (current_pos2 - outcome2 < 0) {
 						current_pos2 = 0;
@@ -81,8 +90,14 @@ public class SnakenLadder {
 
 			}
 		}
+		if (current_pos1 == 100) {
+			System.out.println("player 1 is winner");
+		} 
+		else if (current_pos2 == 100) {
+			System.out.println("player 2 is winner");
+
+		}
 
 	}
 
 }
-
